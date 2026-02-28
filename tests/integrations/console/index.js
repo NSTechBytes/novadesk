ccadfd7b7c05f9021980ef89a8ea65e2964a6263
nstechbytes <nstechbytes@gmail.com>
2026-02-28 13:31:52 +0500

    fix(console): correct typo in debugging enablement code.

--- tests/integrations/console/index.js
+++ tests/integrations/console/index.js
@@ -1,6 +1,6 @@
 import { app } from "novadesk";
-// app.enableDebugging(true);
-const enableDebugging = app.enableDebuggin(true);
+
+const enableDebugging = app.enableDebugging(true);
 
 if (enableDebugging) {
   console.log("Debugging is enabled");
