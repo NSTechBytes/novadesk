7a2503d40a74b2f2ec2eedd9dcdee9418750fceb
nstechbytes <nstechbytes@gmail.com>
2026-03-06 05:58:30 +0500

    refactor(widgets): rename UI script file and update import path.

--- src/Widgets/template/index.js
+++ src/Widgets/template/index.js
@@ -1,7 +1,9 @@
+import widgetWindow from "novadesk";
+
 var myWindow = new widgetWindow({
     id: "myWindow",
     width: 200,
     height: 200,
-    script: "ui/ui.js",
+    script: "ui/script.ui.js",
     backgroundColor:"#ffffffff",
 });
